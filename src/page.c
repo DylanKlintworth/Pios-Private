814838153838357fdcc92223901486a04a36ddef
Dylan Klintworth <dylan.klintworth@gmail.com>
2021-03-16 13:11:53 -0500

    changed freeing and allocation

--- src/page.c
+++ src/page.c
@@ -2,41 +2,45 @@
 #include "null.h"
 #define NUMBERPAGES 128
 
-extern 
+void ppageAdd(ppage *head, ppage *newElement){
 
-void ppageAdd(ppage **head, ppage *newElement){
+    ppage *iterator = (ppage *) head;
 
-    ppage *iterator = (ppage*) head;
-
-    newElement->next = iterator->next;
-    newElement->prev = iterator;
-
-    iterator->next = newElement;
+    // Link element b into the list between iterator and iterator->next.
     
-    if(newElement->next != NULL){
-        newElement->next->prev = newElement;
+    while(iterator->next != NULL){
+        iterator = iterator->next;
     }
+    iterator->next = newElement;
+    newElement->prev = iterator;
 }
 
-void ppageRemove(ppage *b){
-    if(b->next != NULL){
-		b->next->prev = b->prev;
+ppage* ppageRemove(ppage *b){
+
+    ppage *iterator = b;
+    
+    while(iterator->next != NULL){
+        iterator = iterator->next;
     }
-	b->prev->next = b->next;
-	b->next = NULL;
-	b->prev = NULL;
+    iterator = iterator->prev;
+    iterator->next->prev = NULL;
+    ppage *temp = iterator->next;
+    iterator->next = NULL;
+	return temp;
 }
 
 
 ppage physical_page_array[NUMBERPAGES]; //128 pages of 2MB each = 256MB of pages
-ppage *free_pages_list = NULL; //Initial free page list
+ppage *free_pages_list = &physical_page_array[0]; //Initial free page list
 
 void init_pfa_list(void){
-
-    void *pages_address = 0x200000; //0x200000 is 2MB (16^5); first physical address
+    free_pages_list = &physical_page_array[0]; //free_pages_list needs pointer to start list
+    void *pages_address = (void *) 0x200000; //0x200000 is 2MB (16^5); first physical address
+    physical_page_array[0].physical_addr = pages_address;
+    pages_address += 0x200000;
     //create head for free_pages_list - initial allocation
     int i;
-    for (i = 0; i < NUMBERPAGES; i++){
+    for (i = 1; i < NUMBERPAGES; i++){
         physical_page_array[i].physical_addr = pages_address; //give page physical address equal to pages_address (multiple of 2MB)
         ppageAdd(free_pages_list, &physical_page_array[i]); //add physical page to free pages linkedlist
         pages_address = pages_address + 0x200000; //increment pages address by 2MB
@@ -44,17 +48,23 @@ void init_pfa_list(void){
 }
 
 void free_physical_pages(ppage *ppage_list){
-    
+    ppageAdd(free_pages_list, ppage_list); //adds ppage list to end of free_page_list
 }
 
 ppage *allocate_physical_pages(unsigned int npages){
-    ppage *allocd_list = NULL; //list of allocated pages to be returned
-    ppage *initial = free_pages_list; //start at initial node of free list
+    ppage *allocd_list; //list of allocated pages to be returned
+    ppage *pgs[npages];
+
     int i = 0;
-    while (i < npages){ //remove and allocated npages
-        ppageRemove(initial); //remove node from free_pages_list (next & previous removed)
-        ppageAdd(allocd_list, initial); //add node to allocd_list
+    while (i < npages){
+        pgs[i] = ppageRemove(free_pages_list);
         i++;
     }
+    int j;
+    allocd_list = pgs[0];
+    for (j = 1; j < npages; j++){
+        ppageAdd(allocd_list, pgs[j]);
+    }
+
     return allocd_list; //return new list of physical pages
 }
--- src/page.h
+++ src/page.h
@@ -8,8 +8,8 @@
 
 	typedef struct ppage ppage;
 
-	void ppageAdd(ppage **head, ppage *newElement);
-	void ppageRemove(ppage *b);
+	void ppageAdd(ppage *head, ppage *newElement);
+	ppage *ppageRemove(ppage *b);
 	void init_pfa_list(void);
 	void free_physical_pages(ppage *ppage_list);
 	ppage *allocate_physical_pages(unsigned int npages);
