f36c74eba2bf8db7ba666ba30457f447e0febdfe
DylanKlintworth <dylan.klintworth@gmail.com>
2021-03-28 13:25:53 -0500

    changed read printing formatting

--- src/kernel_main.c
+++ src/kernel_main.c
@@ -9,7 +9,7 @@ extern long __bss_end;
 void clear_bss();
 void kernel_main() {
 	file fle0, fle1, fle2, fle3, fle4;
-	char buffer0[100000];
+	char buffer0[30000];
 	char buffer[11000];
 	char buffer2[100];
 	char buffer3[200];
@@ -23,29 +23,18 @@ void kernel_main() {
 	fatOpen(&fle2, "/dylan");
 	fatOpen(&fle3, "/hello.txt");
 	fatOpen(&fle4, "/alan/newfile");
-	fatRead(buffer0, &fle0, 100000);
+	fatRead(buffer0, &fle0, 30000);
 	fatRead(buffer, &fle1, 11000);
 	fatRead(buffer2, &fle2, 50);
 	fatRead(buffer3, &fle3, 50);
 	fatRead(buffer4, &fle4, 100);
-	int i;
-	for (i = 0; i < strlen(buffer); i++){
-		esp_printf((void * ) putc, "%c", buffer[i]);
-	}
-	esp_printf((void * ) putc, "\n");
-	for (i = 0; i < strlen(buffer2); i++){
-		esp_printf((void * ) putc, "%c", buffer2[i]);
-	}
-	esp_printf((void * ) putc, "\n");
-	for (i = 0; i < strlen(buffer3); i++){
-		esp_printf((void * ) putc, "%c", buffer3[i]);
-	}
-	for (i = 0; i < strlen(buffer4); i++){
-		esp_printf((void * ) putc, "%c", buffer4[i]);
-	}
-	esp_printf((void * ) putc, "\n");
-	for (i = 0; i < strlen(buffer0); i++){
-		esp_printf((void * ) putc, "%c", buffer0[i]);
+	esp_printf((void * ) putc, "%s\n", buffer0);
+	esp_printf((void * ) putc, "%s\n", buffer);
+	esp_printf((void * ) putc, "%s\n", buffer2);
+	esp_printf((void * ) putc, "%s\n", buffer3);
+	esp_printf((void * ) putc, "%s\n", buffer4);
+	while (1){
+		
 	}
 }
 
