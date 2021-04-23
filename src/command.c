93aa8a796f7f63bbc9d5da30ea2187ffe6fa067f
Dylan Klintworth <dylan.klintworth@gmail.com>
2021-04-23 15:05:50 -0500

    added breaks to switch in touch command

--- src/command.c
+++ src/command.c
@@ -13,16 +13,22 @@ int touch(char path[]){
     switch (result){
         case 1:
             esp_printf((void *) putc, "File already exists.\n");
+            break;
         case 2:
             esp_printf((void *) putc, "Subdirectory not found.\n");
+            break;
         case 3:
             esp_printf((void *) putc, "File exists in subdirectory.\n");
+            break;
         case 4:
             esp_printf((void *) putc, "File could not be created.\n");
+            break;
         case 5:
             esp_printf((void *) putc, "File path given is not valid\n");
+            break;
         default:
             esp_printf((void *) putc, "File created.\n");
+            break;
     }
     return result;
 }
