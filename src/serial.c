fbcdedbd7df09867fd9e9de6afc0044c2424ff32
Dylan Klintworth <dylan.klintworth@gmail.com>
2021-04-24 23:03:33 -0500

    working on uart implementation

--- src/serial.c
+++ src/serial.c
@@ -5,3 +5,8 @@ void putc(int data){
 	uint32_t *mu_io	= (uint32_t *) RPI3_MU_IO;
 	*mu_io = data;
 }
+
+void getc(char buffer[]){
+	uint32_t *mu_io	= (uint32_t *) RPI3_MU_IO;
+	buffer[0] = *mu_io;
+}
--- src/serial.h
+++ src/serial.h
@@ -3,4 +3,5 @@
 	#define RPI3_MU_IO 0x3F215040
 	#define RPI4_MU_IO 0xFE215040
 	void putc(int data);
+	void getc(char buffer[]);
 #endif
